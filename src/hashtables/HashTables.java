74acde5dcbda6db6f5970502a63c9630521c59e9
Lubzie1007 <lubznotutela17@gmail.com>
2024-02-25 13:58:53 +0200

    practice

---
+++ src/hashtables/HashTables.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hashtables;
+
+import java.util.Hashtable;
+
+
+
+/**
+ *
+ * @author Lubabalo Notutela
+ */
+public class HashTables {
+    public static void main(String[] args) {
+        Hashtable<Integer, String> ht1 = new Hashtable<>();
+        
+        Hashtable<Integer, String> ht2 = new Hashtable<Integer, String>();
+        
+        ht1.put(1, "one");
+        ht1.put(2, "two");
+        ht1.put(3, "three");
+        
+        ht2.put(4, "four");
+        ht2.put(5, "five");
+        ht2.put(6, "six");
+        
+        System.out.println("The Hashtable for ht1: " +ht1);
+        System.out.println("The Hashtable for ht2: " +ht2);
+    }
+    
+}
